4bd511f7697b44931d20d077be86f22f52206a92
ralet <arnaud.ralet@fnherstal.com>
2025-10-28 14:58:24 +0100

    Fix age group boundaries to prevent aging issues at 64-65

--- data/ageGroups.js
+++ data/ageGroups.js
@@ -3,6 +3,6 @@ const AGE_GROUPS = {
   BABY: { min: 0, max: 2, deathChance: 0.02, name: "Baby" },
   CHILD: { min: 3, max: 12, deathChance: 0.005, name: "Child" },
   TEENAGE: { min: 13, max: 19, deathChance: 0.003, name: "Teenager" },
-  ADULT: { min: 20, max: 64, deathChance: 0.001, name: "Adult" },
-  ELDER: { min: 65, max: 200, deathChance: 0.01, name: "Elder" }
+  ADULT: { min: 20, max: 65, deathChance: 0.001, name: "Adult" },
+  ELDER: { min: 66, max: 200, deathChance: 0.01, name: "Elder" }
 };
